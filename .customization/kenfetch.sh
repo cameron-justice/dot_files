b60bc516d45e45668a6957d7fc4c309da1275dc5
Cameron Justice <cameronjustice@protonmail.com>
2020-02-29 21:15:46 +0000

    Added customized neofetch script

---
+++ .customization/kenfetch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+GREEN='\e[32m'
+
+echo -e "${GREEN}                                       __"
+echo -e "${GREEN} _  __                    _  _         \\ \\__"
+echo -e "${GREEN}| |/ /                   | |(_)         > __)"
+echo -e "${GREEN}| ' /  ___  _ __    __ _ | | _   __ _  ( (_"
+echo -e "${GREEN}|  <  / _ \\| '_ \\  / _\` || || | / _\` |  > _)"
+echo -e "${GREEN}| . \\|  __/| | | || (_| || || || (_| | ( (__"
+echo -e "${GREEN}|_|\\_\\\\___||_| |_| \\__,_||_||_| \\__,_|  \\__ \\"
+echo -e "${GREEN}                                           ) )"
+echo -e "${GREEN}                                          (_/\e[0m"
