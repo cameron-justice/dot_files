38691ee55d817f2bb61ba32ca656db8bb4433f78
cameron-justice <cameronjustice@protonmail.com>
2020-03-02 12:38:28 -0600

    Updated spacings on kenfetch.sh

--- .customization/kenfetch.sh
+++ .customization/kenfetch.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 
-GREEN='\e[32m'
+BLUE='\e[34m'
 
-echo -e "${GREEN}                                       __"
-echo -e "${GREEN} _  __                    _  _         \\ \\__"
-echo -e "${GREEN}| |/ /                   | |(_)         > __)"
-echo -e "${GREEN}| ' /  ___  _ __    __ _ | | _   __ _  ( (_"
-echo -e "${GREEN}|  <  / _ \\| '_ \\  / _\` || || | / _\` |  > _)"
-echo -e "${GREEN}| . \\|  __/| | | || (_| || || || (_| | ( (__"
-echo -e "${GREEN}|_|\\_\\\\___||_| |_| \\__,_||_||_| \\__,_|  \\__ \\"
-echo -e "${GREEN}                                           ) )"
-echo -e "${GREEN}                                          (_/\e[0m"
+echo -e "${BLUE}                                       __"
+echo -e "${BLUE} _  __                    _  _         \\ \\__"
+echo -e "${BLUE}| |/ /                   | |(_)         > __)"
+echo -e "${BLUE}| ' /  ___  _ __    __ _ | | _   __ _  ( (_"
+echo -e "${BLUE}|  <  / _ \\| '_ \\  / _\` || || | / _\` |  > _)"
+echo -e "${BLUE}| . \\|  __/| | | || (_| || || || (_| | ( (__"
+echo -e "${BLUE}|_|\_\\\\\___||_| |_| \\__,_||_||_| \\__,_|  \\__ \\"
+echo -e "${BLUE}                                           ) )"
+echo -e "${BLUE}                                          (_/\e[0m"
+echo -e "\n"
+neofetch
